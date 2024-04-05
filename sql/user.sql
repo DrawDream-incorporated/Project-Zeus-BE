3f93b5fe0ec1e547762b173297ab3b1407298685
kckoh <kyungcheolkoh@gmail.com>
2024-04-04 18:48:14 -0700

    indentation fixed for user sql

--- sql/user.sql
+++ sql/user.sql
@@ -1,12 +1,12 @@
 CREATE TABLE User (
-                      UserID CHAR(36) NOT NULL UNIQUE,  -- Using CHAR(36) for UUID
-                      Username VARCHAR(20) NOT NULL UNIQUE,
-                      Email VARCHAR(256) PRIMARY KEY,
-                      Password VARCHAR(72) NOT NULL,  -- bcrypt, utf_unicode
-                      FirstName VARCHAR(50),
-                      LastName VARCHAR(50),
-                      Preferred_name VARCHAR(50),
-                      Created_at DATETIME NOT NULL, -- utc
-                      Updated_at DATETIME NOT NULL, -- utc
-                      IsActive BOOLEAN NOT NULL
+    UserID CHAR(36) NOT NULL UNIQUE,  -- Using CHAR(36) for UUID
+    Username VARCHAR(20) NOT NULL UNIQUE,
+    Email VARCHAR(256) PRIMARY KEY,
+    Password VARCHAR(72) NOT NULL,  -- bcrypt, utf_unicode
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Preferred_name VARCHAR(50),
+    Created_at DATETIME NOT NULL, -- utc
+    Updated_at DATETIME NOT NULL, -- utc
+    IsActive BOOLEAN NOT NULL
 );
