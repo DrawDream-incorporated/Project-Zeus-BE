260575c6e6f692d498e3bb9d618e4d2ebb926da1
kckoh <kyungcheolkoh@gmail.com>
2024-04-04 19:35:48 -0700

    modify the sql to add the case sensitivity to the password and add some comments for describe

--- sql/user.sql
+++ sql/user.sql
@@ -1,12 +1,12 @@
 CREATE TABLE User (
-    UserID CHAR(36) NOT NULL UNIQUE,  -- Using CHAR(36) for UUID
-    Username VARCHAR(20) NOT NULL UNIQUE,
-    Email VARCHAR(256) PRIMARY KEY,
-    Password VARCHAR(72) NOT NULL,  -- bcrypt, utf_unicode
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Preferred_name VARCHAR(50),
-    Created_at DATETIME NOT NULL, -- utc
-    Updated_at DATETIME NOT NULL, -- utc
-    IsActive BOOLEAN NOT NULL
+    user_id CHAR(36) NOT NULL UNIQUE COMMENT 'UUID',-- Using CHAR(36) for UUID
+    username VARCHAR(20) NOT NULL UNIQUE COMMENT 'It is equal to ID in Konglish',
+    email VARCHAR(256) PRIMARY KEY,
+    password VARCHAR(72) NOT NULL COLLATE utf8mb4_0900_as_cs COMMENT 'We use case sensitive and Accent Sensitive',  -- bcrypt, utf_unicode
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    preferred_name VARCHAR(50) COMMENT 'Displayed Name for the user',
+    created_at DATETIME NOT NULL COMMENT 'UTC timzezone', -- UTC
+    updated_at DATETIME NOT NULL COMMENT 'UTC timzezone', -- UTC
+    is_active BOOLEAN NOT NULL
 );
