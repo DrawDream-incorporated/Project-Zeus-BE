6c7b3f3e139094167901836381d4091573cac34c
Youjeong Park <>
2024-06-23 13:21:09 -0700

    feat : logging

--- src/main/java/com/konada/Konada/controller/TestController.java
+++ src/main/java/com/konada/Konada/controller/TestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
         @GetMapping("/test")
         public String hello() {
+            log.info("Received request for /test endpoint");
             return "Hello, World!";
         }
 
